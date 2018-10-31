c3569ecf6d06824b7d6d69f18c88969c5485f5de
CandicXiong <2029389910@qq.com>
2018-10-31 13:36:22 +0800

    第一次提交

---
+++ src/main/java/com/example/baisekuangjia/BaisekuangjiaApplication.java
@@ -0,0 +1,12 @@
+package com.example.baisekuangjia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaisekuangjiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run (BaisekuangjiaApplication.class, args);
+    }
+}
